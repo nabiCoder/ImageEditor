96ca180f4beb6781abed2a2609a1571f84807616
nabiCoder <nabiulli_n@vk.com>
2025-08-12 11:18:52 +0500

    Initial commit

---
+++ ImageEditorTest/ImageEditorTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageEditorTestApp.swift
+//  ImageEditorTest
+//
+//  Created by Денис Набиуллин on 12.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct ImageEditorTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
